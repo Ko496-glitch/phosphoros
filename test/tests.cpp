b27f7dc9c5ee40929b405a5535031613bae1c31b
Kartik Ohlan <kartik7ohlan@gmail.com>
2026-04-18 17:34:45 -0400

    Added Catch2 tests for exceptions throw

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,21 @@
 #include <catch2/catch_test_macros.hpp>
+#include<libkdb/process.hpp>
+#include<signal.h>
+#include<sys/types.h>
+#include<libkdb/error.hpp>
 
-TEST_CASE("validate environment") {
-    REQUIRE(false);
+namespace{
+    bool process_exist(pid_t pid){
+        auto sig = kill(pid,0);
+        return (sig != -1 && errno != ESRCH);
+    }
+}//namespace
+
+TEST_CASE("process::launch success", "[process]"){
+    auto proc =  kdb::process::launch("yes");
+    REQUIRE(process_exist(proc->pid()));
+}
+
+TEST_CASE("process::launc no_such_program", "[process]"){
+    REQUIRE_THROWS_AS(kdb::process::launch("this_should_not_work"), kdb::error);
 }
