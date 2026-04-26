31fc8e75acdeac969dec8016997450076471f942
Kartik Ohlan <kartik7ohlan@gmail.com>
2026-04-25 23:05:33 -0400

    Added HAL for ptrace

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kartikohlan/phosphoros/kdb
-# Build directory: /Users/kartikohlan/phosphoros/kdb/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tools")
-subdirs("test")
