e3bd7e20af032c57d272aa73e421f7010c3d75ab
Kartik Ohlan <kartik7ohlan@gmail.com>
2026-05-11 22:58:19 -0400

    Remove build artifacts from repository

--- build/test/targets/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kartikohlan/phosphoros/kdb/test/targets
-# Build directory: /Users/kartikohlan/phosphoros/kdb/build/test/targets
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
